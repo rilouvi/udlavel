8a11428d625421eb7f79cbcb16141b5b6391fb43
rilouvi <supriyonoputra3@gmail.com>
2018-10-15 01:14:36 +0700

    Perapian dan widget

--- resources/views/backend/blog/create.blade.php
+++ resources/views/backend/blog/create.blade.php
@@ -28,15 +28,21 @@
         <!-- Main content -->
         <section class="content">
             <div class="row">
-              <div class="col-xs-12">
+              {!! Form::model($post,[
+                  'method'=>'POST',
+                  'route'=>'blog.store',
+                  'files'=>TRUE,
+                  'id'=>'post-form'
+              ]) !!}
+              <div class="col-xs-9 mr-0">
                 <div class="box">
                   <!-- /.box-header -->
                   <div class="box-body ">
-                     {!! Form::model($post,[
+                     {{-- {!! Form::model($post,[
                          'method'=>'POST',
                          'route'=>'blog.store',
                          'files'=>TRUE
-                     ]) !!}
+                     ]) !!} --}}
 
                      <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
                        {!! Form::label('title') !!}
@@ -52,7 +58,7 @@
                           <span class="help-block">{{ $errors->first('slug') }}</span>
                         @endif
                     </div>
-                    <div class="form-group {{ $errors->has('excerpt') ? 'has-error' : '' }}">
+                    <div class="form-group excerpt {{ $errors->has('excerpt') ? 'has-error' : '' }}">
                       {!! Form::label('excerpt') !!}
                       {!! Form::textarea('excerpt', null, ['class'=>'form-control']) !!}
                         @if ($errors->has('excerpt'))
@@ -66,13 +72,47 @@
                           <span class="help-block">{{ $errors->first('body') }}</span>
                         @endif
                     </div>
+                    {{-- {!! Form::submit('Create Post',['class'=>'btn btn-primary']) !!} --}}
+
+                     {{-- {!! Form::close() !!} --}}
+                  </div>
+                  <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+              </div>
+              <div class="col-xs-3">
+                <div class="box">
+                  <div class="box-header  with-border">
+                    <h3 class="box-title">Publish</h3>
+                  </div>
+                  <div class="box-body">
                     <div class="form-group {{ $errors->has('published_at') ? 'has-error' : '' }}">
                       {!! Form::label('published_at','Published Date') !!}
-                      {!! Form::text('published_at', null, ['class'=>'form-control','placeholder'=>'Y-m-d H:i:s']) !!}
+                      <div class='input-group date' id='datetimepicker1'>
+                        {!! Form::text('published_at', null, ['class'=>'form-control','placeholder'=>'Y-m-d H:i:s']) !!}
+                        <span class="input-group-addon">
+                            <span class="glyphicon glyphicon-calendar"></span>
+                        </span>
+                      </div>
                         @if ($errors->has('published_at'))
                           <span class="help-block">{{ $errors->first('published_at') }}</span>
                         @endif
                     </div>
+                  </div>
+                  <div class="box-footer clearfix">
+                    <div class="pull-left">
+                      <a id="draft-btn" class="btn btn-default">Save Draft</a>
+                    </div>
+                    <div class="pull-right">
+                      {!! Form::submit('Publish',['class'=>'btn btn-primary']) !!}
+                    </div>
+                  </div>
+                </div>
+                <div class="box">
+                  <div class="box-header with-border">
+                    <h3 class="box-title">Category</h3>
+                  </div>
+                  <div class="box-body">
                     <div class="form-group {{ $errors->has('category_id') ? 'has-error' : '' }}">
                       {!! Form::label('category_id','Category') !!}
                       {!! Form::select('category_id', App\Category::pluck('title','id'), null, ['class'=>'form-control','placeholder'=>'Choose Category']) !!}
@@ -80,22 +120,35 @@
                           <span class="help-block">{{ $errors->first('category_id') }}</span>
                         @endif
                     </div>
+                  </div>
+                </div>
+                <div class="box">
+                  <div class="box-header with-border">
+                    <h3 class="box-title">Feature Image</h3>
+                  </div>
+                  <div class="box-body text-center">
                     <div class="form-group {{ $errors->has('images') ? 'has-error' : '' }}">
                       {!! Form::label('images','Feature Image') !!}
-                      {!! Form::file('images') !!}
+                      <br>
+                      <div class="fileinput fileinput-new" data-provides="fileinput">
+                        <div class="fileinput-new thumbnail" style="width: 200px; height: 150px;">
+                          <img src="https://via.placeholder.com/200x150&text=No+Image" alt="...">
+                        </div>
+                        <div class="fileinput-preview fileinput-exists thumbnail" style="max-width: 200px; max-height: 150px;"></div>
+                        <div>
+                          <span class="btn btn-default btn-file"><span class="fileinput-new">Select image</span><span class="fileinput-exists">Change</span>{!! Form::file('images') !!}</span>
+                          <a href="#" class="btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
+                        </div>
+                      </div>
+                      
                         @if ($errors->has('images'))
                           <span class="help-block">{{ $errors->first('images') }}</span>
                         @endif
-                    </div>                    
-
-                    {!! Form::submit('Create Post',['class'=>'btn btn-primary']) !!}
-
-                     {!! Form::close() !!}
+                    </div>  
                   </div>
-                  <!-- /.box-body -->
                 </div>
-                <!-- /.box -->
               </div>
+              {!! Form::close() !!}
             </div>
           <!-- ./row -->
         </section>
@@ -107,5 +160,30 @@
 @section('script')
     <script type="text/javascript">
         $('ul.pagination').addClass('no-margin pagination-sm');
+
+        $('#title').on('blur',function() {
+          var theTitle = this.value.toLowerCase().trim(),
+          slugInput = $('#slug'),
+          theSlug = theTitle.replace(/&/g, '-and-')
+            .replace(/[^a-z0-9-]+/g, '-')
+            .replace(/\-\-+/g, '-')
+            .replace(/^-+|-+$/g, '');
+          slugInput.val(theSlug);
+        });
+
+        var simplemde1 = new SimpleMDE({ element: $("#excerpt")[0] });
+        var simplemde2 = new SimpleMDE({ element: $("#body")[0] });
+
+        $('#datetimepicker1').datetimepicker({
+          format: 'YYYY-MM-DD HH:mm:ss',
+          showClear:true 
+        });
+
+        $('#draft-btn').click(function(e){
+          e.preventDefault();
+          // \\ Bikin error karena tidak ada value //\\
+          // $('#published_at').val("");
+          $('#post-form').submit();
+        });
     </script>
 @endsection
